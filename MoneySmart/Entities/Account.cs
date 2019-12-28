da3ebcb50ddf2aa92072ce5829f6ee0bbb490a1e
Felipe Romero <feliperomero3@outlook.com>
2019-12-27 23:28:01 -0600

    Add domain Entities.

---
+++ MoneySmart/Entities/Account.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace MoneySmart.Entities
+{
+    public class Account : Entity
+    {
+        public Account()
+        {
+            Transactions = new List<Transaction>();
+        }
+
+        public string Name { get; set; }
+        public double Balance { get; set; }
+
+        public ICollection<Transaction> Transactions { get; }
+
+    }
+}
