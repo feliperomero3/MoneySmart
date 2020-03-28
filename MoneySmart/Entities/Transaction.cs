162ad98064e12a98f834db67699ccaf2bfe7a456
Felipe Romero <feliperomero3@outlook.com>
2020-03-28 13:54:11 -0600

    Add TransactionType entity (enum).

--- MoneySmart/Entities/Transaction.cs
+++ MoneySmart/Entities/Transaction.cs
@@ -8,7 +8,7 @@ namespace MoneySmart.Entities
         public double Amount { get; set; }
         public DateTime DateTime { get; set; }
         public string Description { get; set; }
-        public string Tipo { get; set; }
+        public TransactionType Type { get; set; }
 
         public Account Account { get; set; }
     }
---
+++ MoneySmart/Entities/TransactionType.cs
@@ -0,0 +1,8 @@
+﻿namespace MoneySmart.Entities
+{
+    public enum TransactionType
+    {
+        Income = 1,
+        Expense = 2
+    }
+}
