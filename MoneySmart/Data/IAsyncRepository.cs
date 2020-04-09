43a699bc8bbaf452cefbe07b964a395bfd700579
Felipe Romero <feliperomero3@outlook.com>
2020-04-08 23:04:06 -0500

    Add generic Repository.

---
+++ MoneySmart/Data/IAsyncRepository.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+using MoneySmart.Entities;
+
+namespace MoneySmart.Data
+{
+    public interface IAsyncRepository<T> where T : Entity
+    {
+        Task<T> GetByIdAsync(long id);
+        Task AddAsync(T entity);
+        Task UpdateAsync(T entity);
+        Task DeleteAsync(T entity);
+    }
+}
---
+++ MoneySmart/Data/Repository.cs
@@ -0,0 +1,38 @@
+﻿using System.Threading.Tasks;
+using MoneySmart.Entities;
+
+namespace MoneySmart.Data
+{
+    public class Repository<T> : IAsyncRepository<T> where T : Entity
+    {
+        private readonly ApplicationDbContext _context;
+
+        protected Repository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<T> GetByIdAsync(long id)
+        {
+            return await _context.Set<T>().FindAsync(id);
+        }
+
+        public async Task AddAsync(T entity)
+        {
+            await _context.Set<T>().AddAsync(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(T entity)
+        {
+            _context.Set<T>().Update(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(T entity)
+        {
+            _context.Set<T>().Remove(entity);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
