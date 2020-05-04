514b5744c5b247e404db6cd31c5289b07ed44d4f
Felipe Romero <feliperomero3@outlook.com>
2020-05-03 20:46:37 -0500

    Reorder Transaction entity properties.

--- MoneySmart/Entities/Transaction.cs
+++ MoneySmart/Entities/Transaction.cs
@@ -5,9 +5,9 @@ namespace MoneySmart.Entities
     public class Transaction : Entity
     {
         public int AccountId { get; set; }
-        public double Amount { get; set; }
         public DateTime DateTime { get; set; }
         public string Description { get; set; }
+        public double Amount { get; set; }
         public TransactionType Type { get; set; }
 
         public Account Account { get; set; }
