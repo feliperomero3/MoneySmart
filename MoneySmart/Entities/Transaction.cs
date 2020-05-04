6b750f23d7d909b55cf7257959f69034d27ed893
Felipe Romero <feliperomero3@outlook.com>
2020-05-03 21:11:08 -0500

    Remove AccountId property from Transaction entity.

--- MoneySmart/Entities/Transaction.cs
+++ MoneySmart/Entities/Transaction.cs
@@ -4,7 +4,6 @@ namespace MoneySmart.Entities
 {
     public class Transaction : Entity
     {
-        public int AccountId { get; set; }
         public DateTime DateTime { get; set; }
         public string Description { get; set; }
         public double Amount { get; set; }
