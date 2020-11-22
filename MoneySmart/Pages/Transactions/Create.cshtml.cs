5bf903ddd465ce83688d58520d85e0939383e53c
Felipe Romero <feliperomero3@outlook.com>
2020-11-21 23:49:46 -0600

    Fix Create Transaction page.

--- MoneySmart/Pages/Transactions/Create.cshtml.cs
+++ MoneySmart/Pages/Transactions/Create.cshtml.cs
@@ -1,14 +1,21 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
 using MoneySmart.Data;
 using MoneySmart.Entities;
+using MoneySmart.Models;
 
 namespace MoneySmart.Pages.Transactions
 {
     public class CreateModel : PageModel
     {
         private readonly ApplicationDbContext _context;
+        public SelectList Accounts;
+        public SelectList TransactionTypes = new SelectList(new[] { "Income", "Expense" });
 
         public CreateModel(ApplicationDbContext context)
         {
@@ -17,20 +24,29 @@ namespace MoneySmart.Pages.Transactions
 
         public IActionResult OnGet()
         {
+            Accounts = new SelectList(_context.Accounts.AsNoTracking().ToList(), "Id", "Name");
+
             return Page();
         }
 
         [BindProperty]
-        public Transaction Transaction { get; set; }
+        public TransactionInputModel TransactionModel { get; set; }
 
         public async Task<IActionResult> OnPostAsync()
         {
             if (!ModelState.IsValid)
             {
+                Accounts = new SelectList(_context.Accounts.AsNoTracking().ToList(), "Id", "Name");
+
                 return Page();
             }
 
-            _context.Transactions.Add(Transaction);
+            var account = _context.Accounts.Find(TransactionModel.AccountId);
+
+            var transaction = new Transaction(TransactionModel.DateTime, account, TransactionModel.Description,
+                (TransactionType)Enum.Parse(typeof(TransactionType), TransactionModel.TransactionTypeName), TransactionModel.Amount);
+
+            _context.Transactions.Add(transaction);
 
             await _context.SaveChangesAsync();
 
