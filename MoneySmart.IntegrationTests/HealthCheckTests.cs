e4852857d6192a1082081082102318290114250f
Felipe Romero <feliperomero3@outlook.com>
2020-11-23 22:36:38 -0600

    Add Integration Test project.

---
+++ MoneySmart.IntegrationTests/HealthCheckTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Xunit;
+
+namespace MoneySmart.IntegrationTests
+{
+    public class HealthCheckTests : IClassFixture<WebApplicationFactory<Startup>>
+    {
+        private readonly HttpClient _httpClient;
+
+        public HealthCheckTests(WebApplicationFactory<Startup> factory)
+        {
+            _httpClient = factory.CreateDefaultClient(new Uri("https://localhost:5001"));
+        }
+
+        [Fact]
+        public async Task HealthCheck_returns_success()
+        {
+            var response = await _httpClient.GetAsync("/healthcheck");
+
+            response.EnsureSuccessStatusCode();
+        }
+    }
+}
