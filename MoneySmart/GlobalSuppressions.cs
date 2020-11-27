769933a01009c1f08e2ebaf342b7c758821753fb
Felipe Romero <feliperomero3@outlook.com>
2020-11-27 00:32:17 -0600

    Suppress CA1052 Design rule message for Program class.

---
+++ MoneySmart/GlobalSuppressions.cs
@@ -0,0 +1,8 @@
+﻿// This file is used by Code Analysis to maintain SuppressMessage
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given
+// a specific target and scoped to a namespace, type, member, etc.
+
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("Design", "CA1052:Static holder types should be Static or NotInheritable", Justification = "@davidfowl: 'It's how console applications always worked.'", Scope = "type", Target = "~T:MoneySmart.Program")]
