3b811357549589ec5eec361b344a76c8360e13c7
Felipe Romero <feliperomero3@outlook.com>
2020-11-28 22:26:05 -0600

    Remove unnecessary 'using' statement.

--- MoneySmart/Data/ApplicationDbSeedData.cs
+++ MoneySmart/Data/ApplicationDbSeedData.cs
@@ -2,7 +2,6 @@
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity;
-using Microsoft.EntityFrameworkCore.Internal;
 using MoneySmart.Entities;
 
 namespace MoneySmart.Data
