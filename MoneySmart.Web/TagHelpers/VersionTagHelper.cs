fc75094044053d4d4fb7e40d41b86c25dbfdfaf7
Felipe Romero <feliperomero3@outlook.com>
2021-10-14 23:35:18 -0500

    Display application version using a custom tag helper.

---
+++ MoneySmart.Web/TagHelpers/VersionTagHelper.cs
@@ -0,0 +1,28 @@
+﻿using System.Linq;
+using System.Reflection;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace MoneySmart.TagHelpers
+{
+    /// <summary>
+    /// Renders the executing assembly information version value
+    /// </summary>
+    [HtmlTargetElement("AssemblyInformationalVersion", TagStructure = TagStructure.NormalOrSelfClosing)]
+    public class VersionTagHelper : TagHelper
+    {
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = string.Empty;
+
+            var attribute = (AssemblyInformationalVersionAttribute)Assembly
+                            .GetExecutingAssembly()
+                            .GetCustomAttributes(typeof(AssemblyInformationalVersionAttribute), false)
+                            .FirstOrDefault();
+
+            if (attribute != null)
+            {
+                output.Content.SetContent(attribute.InformationalVersion);
+            }
+        }
+    }
+}
