6088d3df4be8ab96caf4b5616368ccd4e42655f4
Felipe Romero <feliperomero3@outlook.com>
2022-02-18 23:35:30 -0600

    Add Get_Login_Returns_Success Integration Test.

---
+++ MoneySmart.IntegrationTests/Identity/LoginTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace MoneySmart.IntegrationTests.Identity
+{
+    public class LoginTests : IClassFixture<CustomWebApplicationFactory<Startup>>
+    {
+        private readonly CustomWebApplicationFactory<Startup> _factory;
+
+        public LoginTests(CustomWebApplicationFactory<Startup> factory)
+        {
+            _factory = factory;
+            _factory.ClientOptions.AllowAutoRedirect = false;
+            _factory.ClientOptions.BaseAddress = new Uri("https://localhost:5001/");
+        }
+
+        [Fact]
+        public async Task Get_Login_Returns_Success()
+        {
+            var client = _factory.CreateClient();
+
+            var response = await client.GetAsync(string.Empty);
+
+            response.EnsureSuccessStatusCode();
+
+            Assert.Equal("text/html; charset=utf-8", response.Content.Headers.ContentType.ToString());
+        }
+    }
+}
