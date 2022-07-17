20a2ae11af76fca8b2538fa5cc4187047393540c
Felipe Romero <feliperomero3@outlook.com>
2022-07-17 01:28:53 -0500

    Remove returnUrl query argument on logout related pages.

--- MoneySmart.Web/Areas/Identity/Pages/Account/Logout.cshtml.cs
+++ MoneySmart.Web/Areas/Identity/Pages/Account/Logout.cshtml.cs
@@ -21,20 +21,15 @@ namespace MoneySmart.Areas.Identity.Pages.Account
             _logger = logger;
         }
 
-        public async Task<IActionResult> OnPost(string returnUrl = null)
+        public async Task<IActionResult> OnPost()
         {
             await _signInManager.SignOutAsync();
+
             _logger.LogInformation("User logged out.");
-            if (returnUrl != null)
-            {
-                return LocalRedirect(returnUrl);
-            }
-            else
-            {
-                // This needs to be a redirect so that the browser performs a new
-                // request and the identity for the user gets updated.
-                return RedirectToPage();
-            }
+            
+            // This needs to be a redirect so that the browser performs a new
+            // request and the identity for the user gets updated.
+            return RedirectToPage();
         }
     }
 }
