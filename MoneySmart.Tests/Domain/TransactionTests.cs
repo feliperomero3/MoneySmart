e35ccb723582af0a5def2e9d5e89a75c73a27d4c
Felipe Romero <feliperomero3@outlook.com>
2022-07-23 22:02:19 -0500

    Update Transaction unit test.

--- MoneySmart.Tests/Domain/TransactionTests.cs
+++ MoneySmart.Tests/Domain/TransactionTests.cs
@@ -7,7 +7,7 @@ namespace MoneySmart.Tests.Domain
     public class TransactionTests
     {
         [Fact]
-        public void Can_create_an_income_transaction()
+        public void Create_transaction()
         {
             // Arrange
             var date = DateTime.Parse("2020-05-08");
@@ -20,24 +20,7 @@ namespace MoneySmart.Tests.Domain
             var sut = new Transaction(date, account, description, transactionType, amount);
 
             // Assert
-            Assert.Equal(TransactionType.Income, sut.TransactionType);
-        }
-
-        [Fact]
-        public void Can_create_an_expense_transaction()
-        {
-            // Arrange
-            var date = DateTime.Parse("2020-05-08");
-            var description = "Payment";
-            var amount = 10;
-            var account = new Account(5575, "TestAccount");
-            var transactionType = TransactionType.Expense;
-
-            // Act
-            var sut = new Transaction(date, account, description, transactionType, amount);
-
-            // Assert
-            Assert.Equal(TransactionType.Expense, sut.TransactionType);
+            Assert.NotNull(sut);
         }
     }
 }
