73eaeeee23f8ebfe32930a86e0e0f53339094034
Felipe Romero <feliperomero3@outlook.com>
2023-07-01 17:36:02 -0600

    Add account name to Transaction details page.
    Fix #106

--- MoneySmart.Web/Pages/Transactions/Index.cshtml.cs
+++ MoneySmart.Web/Pages/Transactions/Index.cshtml.cs
@@ -1,13 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.ComponentModel.DataAnnotations;
+﻿using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using MoneySmart.Data;
-using MoneySmart.Domain;
 
 namespace MoneySmart.Pages.Transactions
 {
@@ -30,41 +26,6 @@ namespace MoneySmart.Pages.Transactions
                 .OrderByDescending(t => t.DateTime)
                 .Select(t => TransactionModel.MapFromTransaction(t))
                 .ToListAsync();
-        }
-    }
-
-    public class TransactionModel
-    {
-        [DisplayName("Number")]
-        public long Id { get; set; }
-
-        [DisplayName("Date")]
-        [DisplayFormat(DataFormatString = "{0:g}")]
-        public DateTime DateTime { get; set; }
-
-        [DisplayName("Account")]
-        public string AccountName { get; set; }
-
-        [DisplayName("Description")]
-        public string Description { get; set; }
-
-        [DisplayName("Type")]
-        public string TransactionType { get; set; }
-
-        [DataType(DataType.Currency)]
-        public decimal Amount { get; set; }
-
-        public static TransactionModel MapFromTransaction(Transaction transaction)
-        {
-            return new TransactionModel
-            {
-                Id = transaction.Id,
-                DateTime = transaction.DateTime,
-                AccountName = transaction.Account.Name,
-                Description = transaction.Description,
-                TransactionType = transaction.TransactionType.ToString(),
-                Amount = transaction.Amount
-            };
         }
     }
 }
---
+++ MoneySmart.Web/Pages/Transactions/TransactionModel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using MoneySmart.Domain;
+
+namespace MoneySmart.Pages.Transactions;
+
+public class TransactionModel
+{
+    [DisplayName("Number")]
+    public long Id { get; set; }
+
+    [DisplayName("Date")]
+    [DisplayFormat(DataFormatString = "{0:g}")]
+    public DateTime DateTime { get; set; }
+
+    [DisplayName("Account")]
+    public string AccountName { get; set; }
+
+    [DisplayName("Description")]
+    public string Description { get; set; }
+
+    [DisplayName("Type")]
+    public string TransactionType { get; set; }
+
+    [DataType(DataType.Currency)]
+    public decimal Amount { get; set; }
+
+    public static TransactionModel MapFromTransaction(Transaction transaction)
+    {
+        return new TransactionModel
+        {
+            Id = transaction.Id,
+            DateTime = transaction.DateTime,
+            AccountName = transaction.Account.Name,
+            Description = transaction.Description,
+            TransactionType = transaction.TransactionType.ToString(),
+            Amount = transaction.Amount
+        };
+    }
+}
