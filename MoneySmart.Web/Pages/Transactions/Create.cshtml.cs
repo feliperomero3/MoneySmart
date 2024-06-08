7c8ce6a9e55a6f0bbf0a70c15538de4e287ecf3d
Felipe Romero <feliperomero3@outlook.com>
2024-06-08 12:23:09 -0600

    Extract TransactionCreateModel type to its own file.

--- MoneySmart.Web/Pages/Transactions/Create.cshtml.cs
+++ MoneySmart.Web/Pages/Transactions/Create.cshtml.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.ComponentModel.DataAnnotations;
+﻿using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
@@ -9,7 +6,6 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using MoneySmart.Data;
-using MoneySmart.Domain;
 using MoneySmart.Telemetry;
 
 namespace MoneySmart.Pages.Transactions
@@ -42,7 +38,7 @@ namespace MoneySmart.Pages.Transactions
         {
             if (!ModelState.IsValid)
             {
-                Accounts = new SelectList(_context.Accounts.AsNoTracking().OrderBy(a => a.Name).ToList(), "Id", "Name",
+                Accounts = new SelectList(await _context.Accounts.AsNoTracking().OrderBy(a => a.Name).ToListAsync(), "Id", "Name",
                     TransactionCreateModel.AccountId);
 
                 return Page();
@@ -59,35 +55,6 @@ namespace MoneySmart.Pages.Transactions
             _telemetryClient.TrackEvent("TransactionCreatedSuccessfully", new Dictionary<string, string>() { { "User", User.Identity.Name } });
 
             return RedirectToPage("./Index");
-        }
-    }
-
-    public class TransactionCreateModel
-    {
-        [Required]
-        [Display(Name = "Date")]
-        public DateTime DateTime { get; set; }
-
-        [Required]
-        [DisplayName("Account")]
-        public long AccountId { get; set; }
-
-        [Required]
-        [DisplayName("Description")]
-        public string Description { get; set; }
-
-        [Required]
-        [DisplayName("Type")]
-        public string TransactionType { get; set; }
-
-        [Required]
-        [DataType(DataType.Currency)]
-        public decimal Amount { get; set; }
-
-        public Transaction MapToTransaction(Account account)
-        {
-            return new Transaction(DateTime, account, Description,
-                (TransactionType)TransactionType, Amount);
         }
     }
 }
---
+++ MoneySmart.Web/Pages/Transactions/TransactionCreateModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using MoneySmart.Domain;
+
+namespace MoneySmart.Pages.Transactions;
+
+public class TransactionCreateModel
+{
+    [Required]
+    [Display(Name = "Date")]
+    public DateTime DateTime { get; set; }
+
+    [Required]
+    [DisplayName("Account")]
+    public long AccountId { get; set; }
+
+    [Required]
+    [DisplayName("Description")]
+    public string Description { get; set; }
+
+    [Required]
+    [DisplayName("Type")]
+    public string TransactionType { get; set; }
+
+    [Required]
+    [DataType(DataType.Currency)]
+    public decimal Amount { get; set; }
+
+    public Transaction MapToTransaction(Account account)
+    {
+        return new Transaction(DateTime, account, Description,
+            (TransactionType)TransactionType, Amount);
+    }
+}
