7135b04994bbdc0b39e796efc0631df8a618e640
Felipe Romero <feliperomero3@outlook.com>
2024-09-14 21:52:21 -0600

    Exclude selected types from code coverage calculations.

--- MoneySmart.Web/Pages/Identity/Account/Login.cshtml.cs
+++ MoneySmart.Web/Pages/Identity/Account/Login.cshtml.cs
@@ -40,14 +40,14 @@ namespace MoneySmart.Pages.Identity.Account
         {
             [Required]
             [EmailAddress]
-            public string Email { get; set; }
+            public string Email { get; init; }
 
             [Required]
             [DataType(DataType.Password)]
-            public string Password { get; set; }
+            public string Password { get; init; }
 
             [Display(Name = "Remember me?")]
-            public bool RememberMe { get; set; }
+            public bool RememberMe { get; init; }
         }
 
         public async Task OnGetAsync(string returnUrl = null)
--- MoneySmart.Web/Pages/Identity/Account/Register.cshtml.cs
+++ MoneySmart.Web/Pages/Identity/Account/Register.cshtml.cs
@@ -4,6 +4,7 @@
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Text;
 using System.Text.Encodings.Web;
@@ -19,6 +20,7 @@ using MoneySmart.Services;
 
 namespace MoneySmart.Pages.Identity.Account
 {
+    [ExcludeFromCodeCoverage]
     public class RegisterModel : PageModel
     {
         private readonly SignInManager<IdentityUser> _signInManager;
@@ -55,18 +57,18 @@ namespace MoneySmart.Pages.Identity.Account
             [Required]
             [EmailAddress]
             [Display(Name = "Email")]
-            public string Email { get; set; }
+            public string Email { get; init; }
 
             [Required]
             [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
             [DataType(DataType.Password)]
             [Display(Name = "Password")]
-            public string Password { get; set; }
+            public string Password { get; init; }
 
             [DataType(DataType.Password)]
             [Display(Name = "Confirm password")]
             [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
-            public string ConfirmPassword { get; set; }
+            public string ConfirmPassword { get; init; }
         }
 
         public async Task OnGetAsync(string returnUrl = null)
