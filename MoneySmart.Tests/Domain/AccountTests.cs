449aa6279601982f6b9e9912b6ff1549d395ae16
Felipe Romero <feliperomero3@outlook.com>
2024-09-21 14:49:58 -0600

    Add edit account unit test.

--- MoneySmart.Tests/Domain/AccountTests.cs
+++ MoneySmart.Tests/Domain/AccountTests.cs
@@ -18,5 +18,25 @@ namespace MoneySmart.Tests.Domain
             // Assert
             Assert.NotNull(sut);
         }
+
+        [Fact]
+        public void Edit_account()
+        {
+            // Arrange
+            const int number = 5575;
+            const string name = "TestAccount";
+            var sut = new Account(number, name);
+
+            const int newNumber = 5576;
+            const string newName = "NewTestAccount";
+            var newAccount = new Account(newNumber, newName);
+
+            // Act
+            sut.EditAccount(newAccount);
+
+            // Assert
+            Assert.Equal(newNumber, sut.Number);
+            Assert.Equal(newName, sut.Name);
+        }
     }
 }
