8ed9f46722b4b1ab01d18ce13c65ce50413691f8
Felipe Romero <feliperomero3@outlook.com>
2024-09-27 00:41:37 -0600

    Wrap transaction card event listener in a IIFE.

--- MoneySmart.Web/wwwroot/js/site.js
+++ MoneySmart.Web/wwwroot/js/site.js
@@ -1,15 +1,16 @@
 ﻿///<reference path="../lib/jquery/jquery.js"/>
 
-$(function() {
-    console.log('Hello from site.js');
-    $('#transactions').on('click', '.card a', function(e) {
-        e.preventDefault();
-        console.log('clicked the transaction card');
-        const id = $(this).data('id');
-        $('#TransactionDetailModal .modal-title').text(`Details #${id}`);
-        $('#TransactionDetailModal .modal-body').load(`?handler=TransactionDetails&id=${id}`);
-        $('#TransactionDetailModal .modal-footer .btn-danger').attr('href', `Transactions/Delete/${id}`);
-        $('#TransactionDetailModal .modal-footer .btn-primary').attr('href', `Transactions/Edit/${id}`);
-        $('#TransactionDetailModal').modal('show');
-    });
+$(function () {
+    (() => {
+        $('#transactions').on('click', '.card a', function (e) {
+            e.preventDefault();
+            console.log('clicked the transaction card');
+            const id = $(this).data('id');
+            $('#TransactionDetailModal .modal-title').text(`Details #${id}`);
+            $('#TransactionDetailModal .modal-body').load(`?handler=TransactionDetails&id=${id}`);
+            $('#TransactionDetailModal .modal-footer .btn-danger').attr('href', `Transactions/Delete/${id}`);
+            $('#TransactionDetailModal .modal-footer .btn-primary').attr('href', `Transactions/Edit/${id}`);
+            $('#TransactionDetailModal').modal('show');
+        });
+    })();
 });
