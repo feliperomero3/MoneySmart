0d671a159ee53a4bfe0dfe24a5915ad0ad3c6ac7
Felipe Romero <feliperomero3@outlook.com>
2024-09-28 21:30:24 -0600

    Include account balance in account details page.

--- MoneySmart.Web/Pages/Accounts/Details.cshtml.cs
+++ MoneySmart.Web/Pages/Accounts/Details.cshtml.cs
@@ -1,39 +1,44 @@
-﻿using System.Threading.Tasks;
+﻿using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using MoneySmart.Data;
 
-namespace MoneySmart.Pages.Accounts
+namespace MoneySmart.Pages.Accounts;
+
+[ResponseCache(VaryByQueryKeys = new[] { "number" }, Duration = 60)]
+public class DetailsModel : PageModel
 {
-    public class DetailsModel : PageModel
+    private readonly ApplicationDbContext _context;
+
+    public DetailsModel(ApplicationDbContext context)
     {
-        private readonly ApplicationDbContext _context;
+        _context = context;
+    }
 
-        public DetailsModel(ApplicationDbContext context)
+    public AccountModel Account { get; set; }
+
+    public async Task<IActionResult> OnGetAsync(long? number)
+    {
+        if (number == null)
         {
-            _context = context;
+            return NotFound();
         }
 
-        public AccountModel Account { get; set; }
+        var account = await _context.Accounts
+            .AsNoTracking()
+            .Include(t => t.Transactions)
+            .OrderBy(a => a.Name)
+            .FirstOrDefaultAsync(m => m.Number == number);
 
-        public async Task<IActionResult> OnGetAsync(long? number)
+        if (account == null)
         {
-            if (number == null)
-            {
-                return NotFound();
-            }
-
-            var account = await _context.Accounts.AsNoTracking().FirstOrDefaultAsync(m => m.Number == number);
-
-            if (account == null)
-            {
-                return NotFound();
-            }
-
-            Account = AccountModel.MapFromAccount(account);
-
-            return Page();
+            return NotFound();
         }
+
+        Account = AccountModel.MapFromAccount(account);
+
+        return Page();
     }
 }
