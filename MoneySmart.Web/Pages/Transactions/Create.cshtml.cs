d9042861779ce81a881f0a5b2711de8b386acbd2
Felipe Romero <feliperomero3@outlook.com>
2024-09-28 21:40:58 -0600

    Add cache to create transaction page.

--- MoneySmart.Web/Pages/Transactions/Create.cshtml.cs
+++ MoneySmart.Web/Pages/Transactions/Create.cshtml.cs
@@ -11,6 +11,7 @@ using MoneySmart.Telemetry;
 
 namespace MoneySmart.Pages.Transactions
 {
+    [ResponseCache(Duration = 60)]
     public class CreateModel : PageModel
     {
         private readonly ApplicationDbContext _context;
