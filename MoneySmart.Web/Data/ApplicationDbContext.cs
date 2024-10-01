affef0562193d68d0a6c0b8a75945fd98cf2f05e
Felipe Romero <feliperomero3@outlook.com>
2024-10-01 14:11:02 -0600

    Add explicit name to transactions table AccountId index.

--- MoneySmart.Web/Data/ApplicationDbContext.cs
+++ MoneySmart.Web/Data/ApplicationDbContext.cs
@@ -36,7 +36,7 @@ namespace MoneySmart.Data
                     .HasConversion(type => (string)type, type => (TransactionType)type)
                     .HasMaxLength(7);
                 t.Property(p => p.Amount).HasColumnType("decimal(8, 2)").IsRequired();
-                t.HasIndex("AccountId");
+                t.HasIndex(new[] { "AccountId" }, "IX_Transactions_AccountId");
                 t.HasOne(p => p.Account).WithMany(p => p.Transactions)
                     .HasForeignKey("AccountId").OnDelete(DeleteBehavior.Restrict);
                 t.HasOne(p => p.Transfer).WithMany(p => p.Transactions)
