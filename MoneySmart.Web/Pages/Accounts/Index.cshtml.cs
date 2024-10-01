e620903a1fd57c8ec0e10b4e42f951a135ad8d7d
Felipe Romero <feliperomero3@outlook.com>
2024-10-01 13:31:15 -0600

    Add cache to accounts page.

--- MoneySmart.Web/Pages/Accounts/Index.cshtml.cs
+++ MoneySmart.Web/Pages/Accounts/Index.cshtml.cs
@@ -1,12 +1,14 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using MoneySmart.Data;
 
 namespace MoneySmart.Pages.Accounts
 {
+    [ResponseCache(Duration = 60)]
     public class IndexModel : PageModel
     {
         private readonly ApplicationDbContext _context;
