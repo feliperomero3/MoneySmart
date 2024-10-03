8910588b44db1f309595fc9c9dad15904a34f03d
Felipe Romero <feliperomero3@outlook.com>
2024-10-03 15:53:45 -0600

    Add XML document to telemetry related types.

--- MoneySmart.Web/Telemetry/ApplicationTelemetry.cs
+++ MoneySmart.Web/Telemetry/ApplicationTelemetry.cs
@@ -4,15 +4,31 @@ using Microsoft.ApplicationInsights;
 
 namespace MoneySmart.Telemetry;
 
+/// <summary>
+/// Send telemetry to the Application Insights service.
+/// </summary>
 public class ApplicationTelemetry : ITelemetryService
 {
     private readonly TelemetryClient _telemetryClient;
 
+    /// <summary>
+    /// Initializes a new instance of the <see cref="ApplicationTelemetry"/> class.
+    /// </summary>
+    /// <param name="telemetryClient">The telemetry client</param>
+    /// <exception cref="ArgumentNullException">Throws if telemetryClient is null</exception>
+    /// <remarks>
+    /// This class is a thin wrapper around the Application Insights TelemetryClient.
+    /// </remarks>
     public ApplicationTelemetry(TelemetryClient telemetryClient)
     {
         _telemetryClient = telemetryClient ?? throw new ArgumentNullException(nameof(telemetryClient));
     }
 
+    /// <summary>
+    /// Track an event with optional properties.
+    /// </summary>
+    /// <param name="eventName">The event name.</param>
+    /// <param name="properties">The (optional) properties.</param>
     public void TrackEvent(string eventName, IDictionary<string, string> properties = null)
     {
         _telemetryClient.TrackEvent(eventName, properties);
--- MoneySmart.Web/Telemetry/ITelemetryService.cs
+++ MoneySmart.Web/Telemetry/ITelemetryService.cs
@@ -2,7 +2,15 @@
 
 namespace MoneySmart.Telemetry;
 
+/// <summary>
+/// Send telemetry to a telemetry service.
+/// </summary>
 public interface ITelemetryService
 {
+    /// <summary>
+    /// Track an event with optional properties.
+    /// </summary>
+    /// <param name="eventName">The event name.</param>
+    /// <param name="properties">The (optional) properties.</param>
     void TrackEvent(string eventName, IDictionary<string, string> properties = null);
 }
