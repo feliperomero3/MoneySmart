92eddbaddefd53d1d05272e50fae6a4bd51c5b36
Felipe Romero <feliperomero3@outlook.com>
2024-10-04 01:10:11 -0600

    Move transaction create model to its corresponding page model.

--- MoneySmart.Web/Pages/Transactions/Create.cshtml.cs
+++ MoneySmart.Web/Pages/Transactions/Create.cshtml.cs
@@ -1,4 +1,7 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
@@ -26,15 +29,47 @@ namespace MoneySmart.Pages.Transactions
         public SelectList Accounts { get; private set; }
         public SelectList TransactionTypes => new(TransactionType.Values, TransactionType.Expense);
 
+        [BindProperty]
+        public TransactionInputModel TransactionCreateModel { get; set; }
+
+        public class TransactionInputModel
+        {
+            [Required]
+            [Display(Name = "Date")]
+            public DateTime DateTime { get; init; }
+
+            [Required]
+            [DisplayName("Account")]
+            public long AccountId { get; init; }
+
+            [Required]
+            [DisplayName("Description")]
+            public string Description { get; init; }
+
+            [Required]
+            [DisplayName("Type")]
+            public string TransactionType { get; init; }
+
+            [Required]
+            [DataType(DataType.Currency)]
+            public decimal Amount { get; init; }
+
+            [DataType(DataType.MultilineText)]
+            public string Note { get; init; }
+
+            public Transaction MapToTransaction(Account account)
+            {
+                return new Transaction(DateTime, account, Description,
+                    (TransactionType)TransactionType, Amount, Note);
+            }
+        }
+
         public async Task<IActionResult> OnGet()
         {
             Accounts = new SelectList(await _context.Accounts.AsNoTracking().OrderBy(a => a.Name).ToListAsync(), "Id", "Name");
 
             return Page();
         }
-
-        [BindProperty]
-        public TransactionCreateModel TransactionCreateModel { get; set; }
 
         public async Task<IActionResult> OnPostAsync()
         {
