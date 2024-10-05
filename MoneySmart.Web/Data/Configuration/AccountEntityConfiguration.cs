d90112fc0beb360b09597130b393129b4717000a
Felipe Romero <feliperomero3@outlook.com>
2024-10-04 23:32:03 -0600

    Update account EF Core entity configuration.

--- MoneySmart.Web/Data/Configuration/AccountEntityConfiguration.cs
+++ MoneySmart.Web/Data/Configuration/AccountEntityConfiguration.cs
@@ -10,7 +10,7 @@ public class AccountEntityConfiguration : IEntityTypeConfiguration<Account>
     {
         builder.ToTable("Accounts").HasKey(k => k.Id);
         builder.Property(p => p.Id).HasColumnName("AccountId");
-        builder.Property(p => p.Name).HasMaxLength(255).IsRequired();
+        builder.Property(p => p.Name).HasMaxLength(256).IsRequired().IsUnicode(false);
         builder.Property(p => p.Number).IsRequired();
         builder.HasIndex(p => new { p.Number }, "IX_Accounts_Number").IsUnique();
     }
