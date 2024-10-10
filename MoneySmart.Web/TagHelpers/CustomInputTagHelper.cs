67415f3f2d9a0aa5692816e5a1327580bbf89725
Felipe Romero <feliperomero3@outlook.com>
2024-10-09 23:52:12 -0600

    Add Custom Input Tag Helper.

---
+++ MoneySmart.Web/TagHelpers/CustomInputTagHelper.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc.TagHelpers;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace MoneySmart.TagHelpers;
+
+/// <summary>
+/// Extend built-in InputTagHelper to add custom behavior.
+/// </summary>
+[HtmlTargetElement("input", Attributes = ForAttributeName, TagStructure = TagStructure.WithoutEndTag)]
+public class CustomInputTagHelper : InputTagHelper
+{
+    private const string ForAttributeName = "asp-for";
+
+    public CustomInputTagHelper(IHtmlGenerator generator) : base(generator)
+    {
+    }
+
+    public override void Process(TagHelperContext context, TagHelperOutput output)
+    {
+        base.Process(context, output);
+
+        // Check if the model type is decimal
+        if (For.ModelExplorer.ModelType == typeof(decimal) || For.ModelExplorer.ModelType == typeof(decimal?))
+        {
+            output.Attributes.SetAttribute("inputmode", "decimal");
+        }
+    }
+}
