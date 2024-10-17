b5b2c3b8bf454458e04ef01112849fb61f0c224e
Felipe Romero <feliperomero3@outlook.com>
2024-10-17 00:00:30 -0600

    Add transactions export functionality.

---
+++ MoneySmart.Web/Pages/Transactions/Export.cshtml.cs
@@ -0,0 +1,89 @@
+using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using MoneySmart.Data;
+using MoneySmart.Domain;
+
+namespace MoneySmart.Pages.Transactions;
+
+public class ExportModel : PageModel
+{
+    private readonly ApplicationDbContext _context;
+    private readonly ILogger<ExportModel> _logger;
+
+    public ExportModel(ApplicationDbContext context, ILogger<ExportModel> logger)
+    {
+        _context = context;
+        _logger = logger;
+    }
+
+    private class TransactionOutputModel
+    {
+        public DateTime DateTime { get; set; }
+
+        public long AccountId { get; set; }
+
+        public string AccountName { get; set; }
+
+        public string Description { get; set; }
+
+        public string TransactionType { get; set; }
+
+        public decimal Amount { get; set; }
+
+        public string Note { get; set; }
+
+        public static TransactionOutputModel MapToTransactionOutputModel(Transaction transaction)
+        {
+            return new TransactionOutputModel
+            {
+                DateTime = transaction.DateTime,
+                AccountId = transaction.Account.Id,
+                AccountName = transaction.Account.Name,
+                Description = transaction.Description,
+                TransactionType = transaction.TransactionType.ToString(),
+                Amount = transaction.Amount,
+                Note = transaction.Note
+            };
+        }
+    }
+
+    public void OnGet()
+    {
+    }
+
+    /// <summary>
+    /// Export transactions to a file.
+    /// </summary>
+    /// <returns>A file with all the transactions.</returns>
+    /// <exception cref="NotImplementedException"></exception>
+    public async Task<FileResult> OnGetFileAsync()
+    {
+        var transactions = await _context.Transactions
+            .AsNoTracking()
+            .Include(t => t.Account)
+            .OrderByDescending(t => t.DateTime)
+            .Select(t => TransactionOutputModel.MapToTransactionOutputModel(t))
+            .ToListAsync();
+
+        _logger.LogDebug("Found {Count} transactions available to export.", transactions.Count);
+
+        var csv = new StringBuilder();
+        csv.AppendLine("DateTime,AccountId,AccountName,Description,TransactionType,Amount,Note");
+
+        foreach (var transaction in transactions)
+        {
+            csv.AppendLine(
+                $"{transaction.DateTime},{transaction.AccountId},{transaction.AccountName},{transaction.Description},{transaction.TransactionType},{transaction.Amount},{transaction.Note}");
+        }
+
+        var bytes = Encoding.Unicode.GetBytes(csv.ToString());
+
+        return File(bytes, "text/csv;charset=utf-16", "transactions.csv");
+    }
+}
