2a9a758bf5a668bd2d9a464399001b66b71be065
Felipe Romero <feliperomero3@outlook.com>
2025-03-13 22:14:19 -0600

    Add UserId column to Accounts table.

--- MoneySmart.Database/dbo/Tables/Accounts.sql
+++ MoneySmart.Database/dbo/Tables/Accounts.sql
@@ -2,6 +2,7 @@
     [AccountId] BIGINT NOT NULL IDENTITY,
     [Number] BIGINT NOT NULL,
     [Name] VARCHAR(256) NOT NULL,
+    [UserId] VARCHAR(450) NULL, 
     CONSTRAINT [PK_Accounts] PRIMARY KEY ([AccountId])
 );
 GO
