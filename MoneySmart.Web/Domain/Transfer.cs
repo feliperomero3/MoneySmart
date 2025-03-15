f0fa05b450bbf07ab987a76916d0afb49bec7653
Felipe Romero <feliperomero3@outlook.com>
2025-03-14 23:40:30 -0600

    Update Transfer to file-scoped namespace.

--- MoneySmart.Web/Domain/Transfer.cs
+++ MoneySmart.Web/Domain/Transfer.cs
@@ -1,15 +1,14 @@
 ﻿using System.Collections.Generic;
 
-namespace MoneySmart.Domain
-{
-    public class Transfer : Entity
-    {
-        public string Notes { get; set; }
-        public ICollection<Transaction> Transactions { get; }
+namespace MoneySmart.Domain;
 
-        public Transfer()
-        {
-            Transactions = new HashSet<Transaction>();
-        }
+public class Transfer : Entity
+{
+    public string Notes { get; set; }
+    public ICollection<Transaction> Transactions { get; }
+
+    public Transfer()
+    {
+        Transactions = new HashSet<Transaction>();
     }
 }
